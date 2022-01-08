619fa526f5ba6b24a7db4c4f2069d76619355f85
zhouyw <zhouyw18087@163.com>
2022-01-08 15:52:19 +0800

    first commit

---
+++ video-demo/src/test/java/com/example/video/VideoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.video;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ websocket-demo/src/main/java/com/example/websocket/config/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.example.websocket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author zhouyw
+ * @date 2022-01-08
+ * @describe com.example.demo.config
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+}
